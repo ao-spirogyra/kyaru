9c28d9ceacb8fe6457f7d9d183ba1bdd8d479b61
satoki <satoki@applenoMacBook-Pro.local>
2019-09-21 16:03:49 +0900

    をrk

--- lib/kyaru/works/convenience_store.rb
+++ lib/kyaru/works/convenience_store.rb
@@ -1,11 +1,22 @@
 #
-#コンビニバイトクラスを定義
+# コンビニバイトクラスを定義
 #
 class Kyaru::ConvenienceStore < AbstractWork
   @name = "コンビニバイト"
   @hourly_wage = 1000
   @working_hours = 5
   @stress_per_hours =2.0
+
+  def apply
+    message
+  end
+
+  def message
+    @bot.message(with_text: 'コンビニバイトしろ') do |event|
+      event.respond 'コンビニバイトします...'
+    end
+  end
+
 end
 
 #test attr_accessorとinherited
